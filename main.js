be74f3610d014725eacdcb79cc8ae3dd5f0f3060
Daniel Palma <danielpalma232@gmail.com>
2022-05-04 21:55:09 -0300

    Dia 3 concluido

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function onScroll() {
-  if(scrollY > 0) {
-    navigation.classList.add('scroll')
+  if (scrollY > 0) {
+    navigation.classList.add('scroll');
   } else {
-    navigation.classList.remove('scroll')
+    navigation.classList.remove('scroll');
   }
 }
 
@@ -13,3 +13,15 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded');
 }
+
+ScrollReveal({ origin: 'top', distance: '30px', duration: 700 }).reveal(
+  `#home, 
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content`,
+);
