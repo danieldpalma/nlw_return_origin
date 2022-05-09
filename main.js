f3a01ed3522e1fb059d9785d700240e792e3bd04
Daniel Palma <danielpalma232@gmail.com>
2022-05-09 00:33:32 -0300

    Dia 5 concluido

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-window.addEventListener('scroll', onScroll)
+window.addEventListener('scroll', onScroll);
 
 onScroll();
 
 function onScroll() {
- showNavOnScroll();
- showBackToTopButtonOnScroll()
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+};
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + (innerHeight / 2);
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  const sectionEndPassedTargetLine = targetLine >= sectionEndsAt;
+  // Limites da sessão
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionID = section.getAttribute('id');
+  const menuElement = document.querySelector(`.menu a[href*=${sectionID}]`)
+
+  menuElement.classList.remove('active');
+  if(sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
@@ -13,7 +36,7 @@ function showNavOnScroll() {
   } else {
     navigation.classList.remove('scroll');
   }
-}
+};
 
 function showBackToTopButtonOnScroll() {
   if (scrollY > 400) {
@@ -21,15 +44,15 @@ function showBackToTopButtonOnScroll() {
   } else {
     backToTopButton.classList.remove('show');
   }
-}
+};
 
 function openMenu() {
   document.body.classList.add('menu-expanded');
-}
+};
 
 function closeMenu() {
   document.body.classList.remove('menu-expanded');
-}
+};
 
 ScrollReveal({ origin: 'top', distance: '30px', duration: 700 }).reveal(
   `#home, 
@@ -40,5 +63,5 @@ ScrollReveal({ origin: 'top', distance: '30px', duration: 700 }).reveal(
   #services .card,
   #about,
   #about header,
-  #about .content`,
+  #about .content`
 );
